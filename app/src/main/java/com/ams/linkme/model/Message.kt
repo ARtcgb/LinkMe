7598bdff3120f5a67a41f09cd2de5fe0f0ef2d24
ARtcgb <71631622+ARtcgb@users.noreply.github.com>
2023-05-16 10:50:15 +0800

    feat: add basic Message and User model

--- app/src/main/java/com/ams/linkme/model/Message.kt
+++ app/src/main/java/com/ams/linkme/model/Message.kt
@@ -1,4 +1,19 @@
 package com.ams.linkme.model
 
-class Message {
-}
+import kotlinx.serialization.Serializable
+
+/**
+ * Message model that represents a message in a chat.
+ *
+ * @param senderId unique identifier for the sender.
+ * @param receiverId unique identifier for the receiver.
+ * @param content the content of the message.
+ * @param timestamp the timestamp of the message.
+ */
+@Serializable
+data class Message(
+    val senderId: String,
+    val receiverId: String,
+    var content: String,
+    var timestamp: Long
+)
--- app/src/main/java/com/ams/linkme/model/User.kt
+++ app/src/main/java/com/ams/linkme/model/User.kt
@@ -1,4 +1,17 @@
 package com.ams.linkme.model
 
-class User {
-}
+import kotlinx.serialization.Serializable
+
+/**
+ * User model that represents a user in the application.
+ *
+ * @param id unique identifier for each user.
+ * @param username the user's username.
+ * @param avatarUrl the url of the user's avatar.
+ */
+@Serializable
+data class User(
+    val id: String,
+    var username: String,
+    var avatarUrl: String
+)
