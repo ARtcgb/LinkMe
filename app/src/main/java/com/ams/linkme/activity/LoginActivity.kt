c92f8cee1a74e091f0bd18d771dda5ade9e20193
ARtcgb <71631622+ARtcgb@users.noreply.github.com>
2023-05-23 15:52:11 +0800

    feat: update LoginActivity.kt

--- app/src/main/java/com/ams/linkme/activity/LoginActivity.kt
+++ app/src/main/java/com/ams/linkme/activity/LoginActivity.kt
@@ -1,4 +1,38 @@
 package com.ams.linkme.activity
 
-class LoginActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.ams.linkme.R
+import com.ams.linkme.databinding.ActivityLoginBinding
+import com.ams.linkme.viewModel.LoginViewModel
+
+/**
+ * LoginActivity is responsible for handling user logins.
+ *
+ * The activity displays login interface with fields for username and password, and a login button.
+ * When the login button is clicked, it will pass the input to LoginViewModel to handle the business logic.
+ */
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var binding: ActivityLoginBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding   = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val username = binding.editTextUsername
+        val password = binding.editTextPassword
+        val button = binding.buttonLogin
+
+        button.setOnClickListener {
+            loginViewModel.logIn(username.text.toString(), password.text.toString())
+        }
+    }
+
 }
