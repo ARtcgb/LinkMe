11a1cd6a1972540a0397a748e562a843e92068b8
ARtcgb <71631622+ARtcgb@users.noreply.github.com>
2023-05-25 10:38:07 +0800

    feat: update api model

---
+++ app/src/main/java/com/ams/linkme/model/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.ams.linkme.model
+
+data class LoginRequest(
+    val username: String,
+    val password: String
+)
---
+++ app/src/main/java/com/ams/linkme/model/LoginResponse.kt
@@ -0,0 +1,6 @@
+package com.ams.linkme.model
+
+data class LoginResponse(
+    val success: Boolean,
+    val message: String
+)
